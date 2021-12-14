53a9eb197b70afad0910680c9f37a94c8fd3637d
Callum Peat <callum.peat@edu.devinci.fr>
2021-12-14 22:53:22 +0100

    exercice 6

---
+++ exercise-6/my_ansible.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+touch meeting_notes.txt
+mv documents 
+cd documents
+cd gamez
+touch csgo.exe
+cd -
+cd work
+cd plannings
+touch april.xlsx
+touch february.xlsx
+touch january.xlsx
+touch march.xlsx
+cd exercise-6
+
